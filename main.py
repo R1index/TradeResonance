ffe7a11aca8eb1367ef1ed1ddfc0327507df2cf2
R1index <131884391+R1index@users.noreply.github.com>
2025-11-03 19:52:14 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 from app import create_app
 
-# Gunicorn entrypoint: gunicorn -w 2 -b 0.0.0.0:${PORT} 'main:create_app()'
 def create_app():
     return _create()
 
-# Flask CLI entry: flask --app main run
 def _create():
     return create_app()
 
+app = create_app()  # ✅ нужно, чтобы gunicorn нашёл "app"
+
 if __name__ == "__main__":
-    app = create_app()
     app.run(host="0.0.0.0", port=8000, debug=True)
